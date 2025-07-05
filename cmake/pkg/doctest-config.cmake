155b401a9d9761dca8913d36c552190d64e9321e
e_gleba <i@egleba.ru>
2025-07-05 23:59:39 +0300

    grande refactor, removed up trash, focused on primary reason

--- cmake/pkg/doctest-config.cmake
+++
@@ -1,9 +0,0 @@
-cpmaddpackage(
-    NAME
-    doctest
-    GITHUB_REPOSITORY
-    doctest/doctest
-    VERSION
-    2.4.11
-    OPTIONS
-    "DOCTEST_WITH_TESTS OFF")
---
+++ src/dll_main.cxx
@@ -0,0 +1,22 @@
+#include "hooks.hxx"
+
+extern "C" BOOL WINAPI DllMain(HINSTANCE hinstDLL,
+                               DWORD     reason,
+                               LPVOID    reserved)
+{
+    switch (reason)
+    {
+        case DLL_PROCESS_ATTACH:
+        {
+            DisableThreadLibraryCalls(hinstDLL); // Optimize DLL loading
+            return memory_defender::initialize();
+        }
+        case DLL_PROCESS_DETACH:
+        {
+            memory_defender::cleanup();
+            return TRUE;
+        }
+        default:
+            return TRUE;
+    }
+}
